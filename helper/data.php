0a887f7e33f3f79b9e68e00a82b9d2eacff0866e
Marcin Trojan <marcin.trojan@emailcenteruk.com>
2017-06-02 15:11:56 +0100

    Create a composer.json and a new helper class
    MXA-325

---
+++ helper/data.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Emailcenter\Maxautomation\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    /**
+     * Path to store config if extension is enabled
+     *
+     * @var string
+     */
+    const XML_PATH_ENABLED = 'emailcenter_maxautomation/general/enabled';
+
+    /**
+     * Check if extension enabled
+     *
+     * @return string|null
+     */
+    public function isEnabled()
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_ENABLED,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+}
